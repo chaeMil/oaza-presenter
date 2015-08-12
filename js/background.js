ee7fa72e1210f9846d89a6b691ca2e148ba70954
Michal Mlejnek <chaemil72@gmail.com>
2015-08-12 13:25:31 +0200

    added morphing to os
    close button on left for linux (ubuntu)

--- js/background.js
+++ js/background.js
@@ -4,36 +4,45 @@
  * @see http://developer.chrome.com/apps/app.runtime.html
  * @see http://developer.chrome.com/apps/app.window.html
  */
+
+var globalData = {};
+ 
+console.log(chrome.runtime.getPlatformInfo.os);
+ 
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  
-  presenterWindow = chrome.app.window.create(
-    'layouts/presenter.html',
-    {
-      id: 'presenter',
-      frame: 'none'
-    },
-    function(win) {
-      win.fullscreen();
-      win.onClosed.addListener(function() {
-        console.log('closed presenter window');
-      });
-    }
-  );
-  
-  mainWindow = chrome.app.window.create(
-    'index.html',
-    {
-      id: 'mainWindow',
-      frame: {
-        color: '#0055FF'
+
+  chrome.runtime.getPlatformInfo(function(info) {
+    globalData.os = info.os;
+    globalData.appId = chrome.runtime.id;
+    
+    presenterWindow = chrome.app.window.create(
+      'layouts/presenter.html',
+      {
+        id: 'presenter',
+        frame: 'none'
+      },
+      function(win) {
+        win.fullscreen();
+        win.onClosed.addListener(function() {
+          console.log('closed presenter window');
+        });
       }
-    }, 
-    function(win) {
-      win.onClosed.addListener(function() {
-        console.log('closed main window');
-        chrome.app.window.get('presenter').close();
-      });
-    }
-  );
-  
+    );
+    
+    mainWindow = chrome.app.window.create(
+      'index.html',
+      {
+        id: 'mainWindow',
+        frame: 'none',
+      }, 
+      function(win) {
+        win.onClosed.addListener(function() {
+          console.log('closed main window');
+          chrome.app.window.get('presenter').close();
+        });
+      }
+    );
+    
+  });
+
 });
--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
-var appId = chrome.runtime.id;
 var presenterFreezed = false;
 var content = $('#content');
+var globalData;
+
+chrome.runtime.getBackgroundPage(function(bgpage) {
+   globalData = bgpage.globalData;
+});
 
 window.onload = function() {
-
+  
   $('#imagesButton').click(function (e) {
     console.log('adding images layout');
     content.load('layouts/images.html', function(e) {
-      
     });
-    
   });
+  
+  if (globalData.os == 'linux') {
+    $('#closeApp').addClass('left');
+    $('#toolbarMenu').addClass('linux');
+  }
   
 };
 
 $(document).on("click", '.setPresenterBg', function(event) { 
-  var file = 'chrome-extension://' + appId + '/' + $(this).data('file');
+  var file = 'chrome-extension://' + globalData.appId + '/' + $(this).data('file');
   console.log('changing presenter background to: ' + file);
   chrome.app.window.get('presenter').contentWindow.changeBg(file);
+});
+
+$(document).on("click", '#closeApp', function(event) { 
+  window.close();
 });
