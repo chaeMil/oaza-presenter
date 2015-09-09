7b11aa0b610d48527c0cc154f6b8add9b14b8e57
Michal Mlejnek <chaemil72@gmail.com>
2015-09-09 17:40:09 +0200

    fixed font size when resizing font for first time

--- js/presenter.js
+++ js/presenter.js
@@ -132,8 +132,8 @@ function setFont(font) {
 
 function setFontSize(size) {
   $('#bibleText').css('font-size', 'calc(5vw * ' + (size / 100) + ')');
-  $('#bibleVerse').css('font-size', 'calc(2vw * ' + (size / 100) + ')');
-  $('#bibleTranslation').css('font-size', 'calc(2vw * ' + (size / 100) + ')');
+  $('#bibleVerse').css('font-size', 'calc(3.5vw * ' + (size / 100) + ')');
+  $('#bibleTranslation').css('font-size', 'calc(3.5vw * ' + (size / 100) + ')');
   renderText();
 }
 
