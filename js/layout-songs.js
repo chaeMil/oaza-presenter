07a601c73e2930b7a837744bd49dcb0e0efe8371
Michal Mlejnek <chaemil72@gmail.com>
2015-09-19 20:37:51 +0200

    edited songs layout

--- js/layout-songs.js
+++ js/layout-songs.js
@@ -2,17 +2,7 @@ function addSongsLayout() {
   $.get("layouts/songs.html", function(data){
     content.append(data);
     
-    $('#songsFolders').jstree({
-      'core' : {
-        'data' : [
-          { "text" : "Root node", "children" : [
-              { "text" : "Child node 1" },
-              { "text" : "Child node 2" }
-            ]
-          }
-        ]
-      }
-    });
+    
     
   });
 }
